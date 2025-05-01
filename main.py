9011823fc4cf7624531ed671d6389ac6e97ad686
Adrian-Hawkins <u21669831@tuks.co.za>
2025-05-01 13:23:12 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import asyncio
-import tornado
+import os
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+import tornado.ioloop
+import tornado.web
+
+from service.http_handlers.HealthHttpHandler import HealthHttpHandler
+from service.http_handlers.StaticPageHttpHandler import AngularStaticFileHandler
+from service.common.logger import SingletonLogger
+
+logger = SingletonLogger()
+SERVICE_PORT = 8888
+
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    default_path = os.path.join("dist", "wood", "browser")
+    paths = [
+        (r"/health", HealthHttpHandler),
+        (r"/(.*)", AngularStaticFileHandler, {
+            "path": default_path,
+            "default_filename": "index.html",
+            "angular_index": "index.html"
+        })
+    ]
+    logger.info("Registering paths")
+    for path in paths:
+        logger.info(f"{path[0]}: {path[1].__name__}")
+
+    app = tornado.web.Application(paths)
+    app.add_handlers(r".*", paths)
+    return app
 
 async def main():
     app = make_app()
-    app.listen(8888)
+    app.listen(SERVICE_PORT)
+    logger.info(f"starting server on {SERVICE_PORT}")
     await asyncio.Event().wait()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
